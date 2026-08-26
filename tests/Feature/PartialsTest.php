4a4ed78e3cb6e97a5a3cffc65ce7a8997fcbfd2b
luanfreitasdev <luanfreitas10@protonmail.com>
2026-08-26 08:44:39 -0300

    Enhance Livewire partials handling and add tests for inline filters

--- tests/Feature/PartialsTest.php
+++ tests/Feature/PartialsTest.php
@@ -35,3 +35,49 @@ it('can increment count via partial render', function () {
         ->call('increment')
         ->assertSet('count', 1);
 });
+
+class TableIgnorePartialsTest extends Component
+{
+    public int $count = 0;
+
+    public function increment(): void
+    {
+        $this->count++;
+
+        partials($this)->partial('table-body', <<<HTML
+<tbody wire:partial="table-body">
+    <tr wire:partial.ignore="filters" wire:key="filters"><td id="filter">keep</td></tr>
+    <tr wire:key="row"><td id="count">{$this->count}</td></tr>
+</tbody>
+HTML);
+    }
+
+    public function render()
+    {
+        return <<<'BLADE'
+            <div>
+                <table>
+                    <tbody wire:partial="table-body">
+                        <tr wire:partial.ignore="filters" wire:key="filters"><td id="filter">keep</td></tr>
+                        <tr wire:key="row"><td id="count">{{ $count }}</td></tr>
+                    </tbody>
+                </table>
+                <button wire:click="increment" id="increment">+</button>
+            </div>
+        BLADE;
+    }
+}
+
+it('emits tbody partialFragments without a full html effect', function () {
+    $test = Livewire::test(TableIgnorePartialsTest::class)
+        ->call('increment');
+
+    expect(data_get($test->effects, 'html'))->toBeNull();
+
+    $html = data_get($test->effects, 'partialFragments.table-body');
+
+    expect($html)->toBeString()
+        ->toContain('wire:partial="table-body"')
+        ->toContain('PARTIAL:IGNORE:filters')
+        ->toContain('id="count">1');
+});
